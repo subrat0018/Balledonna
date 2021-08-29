df9be2a5548bacbe703fe3c092609ed0d4701f16
Harsh Prajapati <harsh.prajapati7890@gmail.com>
2021-08-29 11:49:58 +0530

    added mirror
    added mirror to game and made enemy AI to follow the player

---
+++ Balledonna/Assets/scripts/EnemyAIscrpits/GenerateEnemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemy : MonoBehaviour
+{
+  public GameObject TheEnemy;
+  public int xPos;
+  public int zPos;
+  public int enemyCount=0;
+    void Start()
+    {
+        StartCoroutine(enemyDrop());
+    }
+IEnumerator enemyDrop(){
+    while(enemyCount < 10){
+        xPos =Random.Range(4,27);
+        zPos = Random.Range(-28,25);
+        Debug.Log(zPos);
+        Instantiate(TheEnemy,new Vector3(xPos,1,zPos),Quaternion.identity);
+        yield return new WaitForSeconds(0.2f);
+        enemyCount+=1;
+    }
+}
+    
+}
