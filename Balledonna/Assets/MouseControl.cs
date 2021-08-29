e39c95e4663416c0b712a2d8ff755aa45d7268eb
samarth1029 <87478607+samarth1029@users.noreply.github.com>
2021-08-29 12:16:47 +0545

    x

---
+++ Balledonna/Assets/MouseControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{public float mouseSensitivity=10f;
+
+    float xRotation=0f;
+    float yRotation=0f;
+    [SerializeField] Transform Playerbody;
+    /*void Start()
+    {
+        Cursor.lockState=CursorLockMode.Locked;
+    }*/
+
+    
+    void Update()
+    {
+        float mouseX=Input.GetAxis("Mouse X")*mouseSensitivity;
+        float mouseY=Input.GetAxis("Mouse Y")*mouseSensitivity;
+        xRotation-=mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        yRotation-=mouseX;
+        transform.localRotation=Quaternion.Euler(xRotation,0f,0f);
+        Playerbody.Rotate(Vector3.up * mouseX);
+      
+    }
+}
