efffa7c511c12cd0075ebbcd5717b3370c069eca
Harsh Prajapati <harsh.prajapati7890@gmail.com>
2021-09-12 18:43:25 +0530

    fixed health bar
    fixed health bar

--- Balledonna/Assets/scripts/GunController.cs
+++ Balledonna/Assets/scripts/GunController.cs
@@ -5,17 +5,17 @@ public class GunController : MonoBehaviour
     public GameObject BulletPrefab;
     public float offsetUnitFromFront;
     [SerializeField] Transform bulletPosition;
-    public float Force=100f;
+    public float Force=80f;
     public float BulletThrowingForce;
     void Update()
     {
     if (Input.GetButtonDown("Fire1"))
       {
-            Debug.Log("ok");
+           
             GameObject bullet=Instantiate(BulletPrefab,bulletPosition.transform.position, bulletPosition.transform.rotation);
             bullet.GetComponent<Rigidbody>().AddForce(bulletPosition.transform.forward*BulletThrowingForce);}        
         
-          // changes done
+        
       
     }
 }
